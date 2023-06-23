d73241051e68c613bad42af60cfab0568a3c4c8b
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2023-06-23 16:17:20 +0530

    All combinations of the elements in list

--- source/source/all_combinations/combinations.py
+++ source/source/all_combinations/combinations.py
@@ -1,7 +1,18 @@
 def combinations(mylist):
+  #list reducing technique.
   if len(mylist) == 0:
     return [[]]
 
+  """
+  1. divide list into two parts
+     i.e. first element and rest of the elements.
+  2. pass rest of the elements again to the recursive function
+  3. Get all combinations without first element included 
+  4. Add first element to each combination
+  3. continue step #1 to #4 until the list if divided into the following two parts -
+     (i) first element becomes the last element.
+     (ii) there are no rest of the elements pending (empty list) --> Base condition to return.
+  """
   first_element = mylist[0]
   other_elements = mylist[1:]
 
